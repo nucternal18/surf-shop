e90f8a95e4001422e5e4b810e4ba9ac703ee4467
A Usoro <aolausoro@gmail.com>
2019-08-15 23:53:27 +0100

    Added user model

---
+++ models/post.js
@@ -0,0 +1,12 @@
+/*
+Post
+-title-string
+-price-string
+-description-string
+-images-string
+-location-string
+--latitude-number
+--longitude-number
+-author-object_id(ref users
+-reviews-arrays of objects
+*/
---
+++ models/review.js
@@ -0,0 +1,5 @@
+/*
+Review
+-body-string
+-author-object_id(ref user)
+*/
